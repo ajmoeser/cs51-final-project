33612c4db1d3251f1efe71a9a22e4eeb9a9da429
Alexander Moeser <amoeser@fas.harvard.edu>
2015-04-17 12:17:47 -0400

    adding main.ml

---
+++ main.ml
@@ -0,0 +1,9 @@
+(* Alexander Moeser
+ * CS51 Final Project
+ * 
+ * This file contains the main algorithm for a SAT-solver using the
+ * Davis-Putnam-Logemann-Loveland ("DPLL") algorithm.  Other associated
+ * files contain interfaces and implementations for variables, clauses,
+ * and formulas.  See the ReadMe for details.
+*)
+
